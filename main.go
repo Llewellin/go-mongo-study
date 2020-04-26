6bcda51e9b946cae4baad9cd9b426b1bd5496fe9
Ting Sheng <tingsheng.lee@shopee.com>
2020-04-26 21:40:30 +0800

    saprkles: issue

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 )
 
 var globalDB *mongo.Database
-var account = "appleboy"
+var account = "vincent"
 var ctx context.Context
 var client *mongo.Client
 var session mongo.Session
@@ -53,46 +53,42 @@ func pay(w http.ResponseWriter, r *http.Request) {
 
 	//step 3: subtract current balance and update back to database
 	// err = globalDB.C("bank").UpdateId(entry.ID, &entry)
-	var result *mongo.UpdateResult
+	// var result *mongo.UpdateResult
 	// var result2 mongo.UpdateResult
 
 	// fmt.Println("result = ", result)
 	// fmt.Println("result2 = ", result2)
 
-	for result == nil || result.MatchedCount == 0 {
-		// step 1: get current amount
-		findResult = globalDB.Collection("bank").FindOne(ctx, bson.M{"account": account})
+	// step 1: get current amount
+	findResult = globalDB.Collection("bank").FindOne(ctx, bson.M{"account": account})
 
-		err = findResult.Err()
-		if err != nil {
-			// panic("1")
-			fmt.Println("err = ", err)
-			return
-			// panic(err)
-		}
-
-		err = findResult.Decode(&entry)
-		if err != nil {
-			// panic("2")
-			fmt.Println("2")
-			return
-			// panic(err)
-		}
-		wait := Random(1, 100)
-		time.Sleep(time.Duration(wait) * time.Millisecond)
-
-		entry.Amount = entry.Amount + 1.000
-		entry.Version = entry.Version + 1
-
-		fmt.Println("version = ", entry.Version)
-
-		result, err = globalDB.Collection("bank").UpdateOne(ctx, bson.M{"_id": entry.ID, "version": entry.Version - 1}, bson.M{"$set": bson.M{
-			"amount":  entry.Amount,
-			"version": entry.Version,
-		}})
-		fmt.Println("UpdateOne() result MatchedCount:", result.MatchedCount)
-		// fmt.Println("UpdateOne() result ModifiedCount:", result.ModifiedCount)
+	err = findResult.Err()
+	if err != nil {
+		// panic("1")
+		fmt.Println("err = ", err)
+		return
+		// panic(err)
 	}
+
+	err = findResult.Decode(&entry)
+	if err != nil {
+		// panic("2")
+		fmt.Println("2")
+		return
+		// panic(err)
+	}
+	wait := Random(1, 100)
+	time.Sleep(time.Duration(wait) * time.Millisecond)
+
+	entry.Amount = entry.Amount + 1.000
+
+	// fmt.Println("version = ", entry.Version)
+
+	_, err = globalDB.Collection("bank").UpdateOne(ctx, bson.M{"_id": entry.ID}, bson.M{"$set": bson.M{
+		"amount": entry.Amount,
+	}})
+	// fmt.Println("UpdateOne() result MatchedCount:", result.MatchedCount)
+	// fmt.Println("UpdateOne() result ModifiedCount:", result.ModifiedCount)
 
 	// }
 
